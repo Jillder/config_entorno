53376c04f1784455ec5d94ff1ca82ec15c377205
JordanMeza <jordan0968300@gmail.com>
2023-10-07 22:45:35 -0500

    first commit

---
+++ config_entorno.py
@@ -0,0 +1,12 @@
+"""
+npm init
+tsc -init
+comentar linea 85
+linea 4: "watch": true,
+linea 57 outfile
+comentar linea 84
+linea 108:  },
+  "files": ["inputfiles"]
+install -g http-server
+"start": "http-server -p [puerto]"
+"""
